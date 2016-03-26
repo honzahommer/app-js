af4c0230eb8332d0fae79b7f4f48d8c331532fdc
Jan Hommer <jan.hommer@i-projekt.cz>
2016-03-26 05:35:03 +0100

    Packages.json and Gruntfile.js  skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    // task setup
+    grunt.registerTask('default', []);
+};
